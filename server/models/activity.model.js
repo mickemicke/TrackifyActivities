2a9cc2634a516b4fa91f532ca3989e8a87d5c710
Micke <michael.zetterholm@gmail.com>
2020-10-06 11:48:36 +0200

    better date handling in activites.model and added updatedAt hook

--- server/models/activity.model.js
+++ server/models/activity.model.js
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose");
+const dayjs = require("dayjs");
 
 const ActivitySchema = mongoose.Schema({
   createdAt: {
-    type: Date,
-    default: Date.now,
+    type: String,
+    default: new Date().toISOString(),
     required: true,
   },
-  date: { type: Date },
-  updatedAt: { type: Date, default: Date.now },
+  date: { type: String, required: true, default: dayjs().format("YYYY-MM-DD") },
+  updatedAt: { type: String, default: new Date().toISOString() },
   tags: [], // mixed type, mongoose doesn't auto detect changes, see docs
   details: {}, // mixed type, mongoose doesn't auto detect changes, see docs
   title: String,
@@ -17,6 +18,12 @@ const ActivitySchema = mongoose.Schema({
     required: true,
   },
 });
+
+ActivitySchema.pre("findOneAndUpdate", async (next) => {
+  // updates updatedAt to current timestamp when editing document
+  const newDate = new Date().toISOString();
+  await this.updateOne(this._conditions, { updatedAt: newDate }); // eslint-disable-line
+  next();
 });
 
 module.exports = mongoose.model("Activities", ActivitySchema);
